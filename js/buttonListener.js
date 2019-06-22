5d3cdece1785e5382b6733708ac02b448f24c661
AlexeyMilenkiy <alex.milenkiy@gmail.com>
2019-06-22 12:09:05 +0300

    add checked task

--- js/buttonListener.js
+++ js/buttonListener.js
@@ -1,10 +1,5 @@
 function Button() {}
 
 Button.prototype = {
-    addTask: function() {
-    var buttonAdd = document.querySelector('.add-task');
-        buttonAdd.addEventListener('click', function () {
-            app.render();
-        });
-    }
+
 };
